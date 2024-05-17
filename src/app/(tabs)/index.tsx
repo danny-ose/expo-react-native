258add1b0ace71d75b13cf55675c2584a69c8b80
Daniel <dustincarter8334@gmail.com>
2024-05-17 22:39:35 +0100

    2.ProductListItem component created

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,37 +1,13 @@
-import Colors from "@/src/constants/Colors";
 import products from "@/assets/data/products";
+import ProductListItem from "@/src/components/ProductListItem";
 
-import { StyleSheet, Text, View, Image } from "react-native";
-
-const product = products[0];
+import { View } from "react-native";
 
 export default function TabOneScreen() {
   return (
-    <View style={styles.container}>
-      <Image source={{ uri: product.image }} style={styles.image} />
-      <Text style={styles.title}>{product.name}</Text>
-      <Text style={styles.price}>${product.price}</Text>
+    <View>
+      <ProductListItem product={products[0]} />
+      <ProductListItem product={products[1]} />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    backgroundColor: 'white',
-    padding: 10,
-    borderRadius: 20,
-  },
-  image: {
-    width: '100%',
-    aspectRatio: 1,
-  },
-  title: {
-    fontSize: 18,
-    fontWeight: "600",
-    marginVertical: 10,
-  },
-  price: {
-    color: Colors.light.tint,
-    fontWeight: "bold",
-  },
-});
